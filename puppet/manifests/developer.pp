ff689273987b56bc89cd5b4696108b851c290591
Gregor Gramlich <gramlich@eosc.de>
2011-03-08 10:15:23 +0100

    removed hudson from standard node and modules

--- puppet/manifests/developer.pp
+++ puppet/manifests/developer.pp
@@ -4,7 +4,6 @@ node default inherits 'developernode' {
     include php-dev
     include monodevelop
     include python
-    include hudson
     include ruby
     include pear::php_archive
     include commodities::keymon
