6e538c0b2d8c7ed319420332dfc73bc8a1ddaa98
Gregor Gramlich <gregor@e-mini.gramlich.dyndns.org>
2011-03-25 19:38:59 +0100

    Fixed PHPUnit Dependencies and Pear mechanisms

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -2,9 +2,10 @@ class php {
     package { "php5": ensure => installed }
     package { "php5-cgi": ensure => installed }
     package { "php5-mysql": ensure => installed }
-    package { "php-pear": ensure => installed }
+    package { "php-pear": ensure => latest }
     package { "xsltproc": ensure => installed }
     package { "php5-xsl": ensure => installed }
+    package { "php5-curl": ensure => installed }
     package { "libapache2-mod-php5": ensure => installed, require => Class['apache'] }
     package { "php5-dev": ensure => installed }
     package { "php5-sqlite": ensure => installed, require => Class['sqlite'] }
@@ -25,7 +26,12 @@ class php-dev inherits php {
 class pear {
     class phpunit {
         channel_discover{"pear.symfony-project.com":}
-        channelinstall{"phpunit": channel => 'pear.phpunit.de', name => 'phpunit/PHPUnit'}
+        channel_discover{"components.ez.no":}
+        channelinstall{"phpunit": channel => 'pear.phpunit.de',
+            update => true,
+            name => 'phpunit/PHPUnit',
+            require => [Channel_discover["pear.symfony-project.com", "components.ez.no"]]
+        }
     }
 
     class phing {
@@ -46,10 +52,8 @@ class pear {
     }
 
     class phpcpd {
-        channel_discover{"components.ez.no":}
         install {"phpcpd": name => 'phpunit/phpcpd',
-            require => [Class['phpunit'],
-            Channel_discover['components.ez.no']]
+            require => Class['phpunit']
         }
     }
 
@@ -65,16 +69,16 @@ class pear {
         channelinstall{"pat/vfsStream-alpha": channel => 'pear.php-tools.net'}
     }
 
-    class upgrade_all {
+    define upgrade_all($unless = "false") {
         $channels = 'pear.php.net'
-        channel_update {$channels:}
-        upgrade {"": require => Channel_update[$channels]}
+        channel_update {$channels: unless => $unless}
+        pear_exe {"upgrade-all": require => Channel_update[$channels], unless => $unless}
     }
 
     define channelinstall($channel, $update = false, $alldeps = false) {
         if ($update) {
-            include "upgrade_all"
-            channel_discover {$channel: require => Class['upgrade_all'] }
+            upgrade_all {'all': unless => "pear list $name"}
+            channel_discover {$channel: require => Upgrade_all['all'] }
         } else {
             channel_discover {$channel: }
         }
@@ -93,7 +97,7 @@ class pear {
         }
     }
 
-    define pear_exe($unless = 1) {
+    define pear_exe($unless = "false") {
         exec {"pear_$title":
             command => "pear $name",
             unless => $unless,
@@ -101,8 +105,8 @@ class pear {
         }
     }
 
-    define channel_update() {
-        pear_exe {"channel-update $name":}
+    define channel_update($unless = "false") {
+        pear_exe {"channel-update $name": unless => $unless}
     }
 
     define channel_discover() {
